a6d0009b8987c4efe32639aa3bb2839705d813ea
NiJia <louis70109@gmail.com>
2020-09-09 21:41:34 +0800

    chore: remove unused liff

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 
 import router from "./router/index";
 const app = createApp(App);
-import liff from "@line/liff";
-app.use(liff);
+
 app.use(router);
 app.mount("#app");
