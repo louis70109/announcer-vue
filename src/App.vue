6c3b904ce224536feea4d752a2aee7a9d055f203
NiJia <louis70109@gmail.com>
2020-09-11 17:23:30 +0800

    chore: add jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <div class="jumbotron">
+      <h1 class="display-4">Ann0uncer</h1>
+      <p class="lead">
+        AF is based on Vue3(beta), its backend API service is Announcer, it uses
+        LIFF's shareTargetPicker to send and generate form data to FlexMessage
+        template for LINE User/Group/Room 🙂
+      </p>
+      <a
+        class="btn btn-primary btn-lg"
+        href="https://github.com/louis70109/announcer-vue"
+        role="button"
+        >Learn more</a
+      >
+    </div>
     <router-view />
   </div>
 </template>
