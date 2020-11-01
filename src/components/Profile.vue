0cbfd16a7be1ff728234ab719a3f6ee2c5d36f8b
NiJia <louis70109@gmail.com>
2020-11-01 22:41:12 +0800

    feat: user profile

---
+++ src/components/Profile.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <img
+      :src="user.picture"
+      alt="userPicture"
+      class="img-thumbnail"
+      width="100"
+      height="100"
+    />
+
+    <button class="btn btn-danger" data-testid="logout" @click="logout">
+      Log Out
+    </button>
+  </div>
+</template>
+
+<script>
+import { onMounted, ref } from 'vue';
+
+import liff from '@line/liff';
+
+export default {
+  name: 'Profile',
+  setup() {
+    let user = ref({
+      name: '',
+      picture: '',
+    });
+    onMounted(async () => {
+      await liff.ready;
+      if (!liff.isLoggedIn()) {
+        liff.login();
+      }
+      const profile = await liff.getProfile();
+      user.value.name = profile.displayName;
+      user.value.picture = profile.pictureUrl;
+      console.log(user);
+    });
+    function logout() {
+      liff.logout();
+      window.location.reload();
+    }
+    return { user, logout };
+  },
+};
+</script>
+
+<style>
+.pictureUrl {
+  width: 240px;
+  height: 240px;
+  border-radius: 50%;
+}
+</style>
