62f7d45ebdd859d338aad410f928fa28cce1cbf1
NiJia <louis70109@gmail.com>
2020-11-01 22:41:33 +0800

    chore: format

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-
-import router from "./router/index";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router/index';
 const app = createApp(App);
 
 app.use(router);
-app.mount("#app");
+app.mount('#app');
