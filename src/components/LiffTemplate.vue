bf294dc8d1e4e977be68350311750317985d9154
NiJia <louis70109@gmail.com>
2020-11-01 22:41:46 +0800

    feat: add personal information card

--- src/components/LiffTemplate.vue
+++ src/components/LiffTemplate.vue
@@ -8,6 +8,7 @@
       <option>樣板選擇</option>
       <option>活動表</option>
       <option>名單表</option>
+      <option>個人卡片</option>
     </select>
     <hr />
     <div v-if="form.template == '1'">
@@ -234,6 +235,78 @@
         :value="form.template"
       />
     </div>
+    <div v-else-if="form.template == '3'">
+      <div class="input-group flex-nowrap">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="addon-wrapping">名稱</span>
+        </div>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Input your name"
+          aria-describedby="addon-wrapping"
+          name="title"
+          v-model="form.title"
+        />
+      </div>
+      <div class="input-group flex-nowrap">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="addon-wrapping">大頭貼網址</span>
+        </div>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Input your avatar url e.g. https://open_url.com/xxx.jpg"
+          aria-describedby="addon-wrapping"
+          name="avatar"
+          v-model="form.avatar"
+        />
+      </div>
+      <div class="input-group flex-nowrap">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="addon-wrapping">封面照網址</span>
+        </div>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Input your background url e.g. https://open_url.com/xxx.jpg"
+          aria-describedby="addon-wrapping"
+          name="back"
+          v-model="form.back"
+        />
+      </div>
+      <div class="input-group flex-nowrap">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="addon-wrapping">追隨連結</span>
+        </div>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Input your persional SNS link e.g. https://line.me/xxx"
+          aria-describedby="addon-wrapping"
+          name="followUrl"
+          v-model="form.followUrl"
+        />
+      </div>
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text">描述</span>
+        </div>
+        <textarea
+          class="form-control"
+          aria-label="描述"
+          name="description"
+          v-model="form.description"
+        ></textarea>
+      </div>
+      <input
+        type="hidden"
+        name="template"
+        id="template"
+        :value="form.template"
+      />
+      <br />
+    </div>
     <br />
     <button type="button" class="btn btn-primary" @click="submitTempleteForm">
       送出
@@ -242,34 +315,22 @@
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
-import liff from "@line/liff";
-import qs from "qs";
+import { ref, onMounted } from 'vue';
+import liff from '@line/liff';
+import qs from 'qs';
 export default {
   setup() {
     let form = ref({
-        template: "",
+        template: '',
       }),
       data = {},
       announcer_api = process.env.VUE_APP_API;
     onMounted(async () => {
-      const res = await fetch(`${announcer_api}/liff`);
-
-      try {
-        const r = await res.json();
-        await liff.init({ liffId: r.liffId });
-        if (!liff.isLoggedIn()) {
-          liff.login({ redirectUri: window.location.href });
-          return new Promise.all();
-        }
-      } catch (err) {
-        console.log(`liff.state init error ${err}`);
-      }
       const uri = window.location.search.substring(1);
       const params = new URLSearchParams(uri);
-      const state = params.get("template");
+      const state = params.get('template');
       form.value.template = state;
-      if (state === "2") form.value.people = [{ name: "", time: "" }];
+      if (state === '2') form.value.people = [{ name: '', time: '' }];
     });
 
     async function submitTempleteForm() {
@@ -281,61 +342,70 @@ export default {
         `${announcer_api}/liff/share?${qs.stringify(form.value)}`
       );
       data = await res.json();
-      console.log("data: ", data);
-      if (liff.isApiAvailable("shareTargetPicker")) {
+      console.log('data: ', data);
+      if (liff.isApiAvailable('shareTargetPicker')) {
         try {
           const picker = await liff.shareTargetPicker([JSON.parse(data.flex)]);
           if (picker) {
             // succeeded in sending a message through TargetPicker
             console.log(`[${picker.status}] Message sent!`);
           } else {
-            const [majorVer, minorVer] = (liff.getLineVersion() || "").split(
-              "."
+            const [majorVer, minorVer] = (liff.getLineVersion() || '').split(
+              '.'
             );
             if (parseInt(majorVer) == 10 && parseInt(minorVer) < 11) {
               console.log(
-                "TargetPicker was opened at least. Whether succeeded to send message is unclear"
+                'TargetPicker was opened at least. Whether succeeded to send message is unclear'
               );
-            } else console.log("TargetPicker was closed!");
+            } else console.log('TargetPicker was closed!');
           }
         } catch (error) {
           // something went wrong before sending a message
           console.log(error);
-          console.log("Flex Message got some error");
+          console.log('Flex Message got some error');
           liff.closeWindow();
         }
-      } else console.log("Please login...");
+      } else console.log('Please login...');
     }
     function optionCheck(event) {
-      // init
       switch (event.target.value) {
-        case "活動表":
+        case '活動表':
           form.value = {
-            template: "1",
-            title: "",
-            place: "",
-            time: "",
-            activity: "",
-            url: "",
-            imageUrl: "",
-            desc: "",
+            template: '1',
+            title: '',
+            place: '',
+            time: '',
+            activity: '',
+            url: '',
+            imageUrl: '',
+            desc: '',
           };
           break;
-        case "名單表":
+        case '名單表':
           form.value = {
-            template: "2",
-            title: "",
-            place: "",
-            activity: "",
-            url: "",
-            map: "",
-            people: [{ name: "", time: "" }],
+            template: '2',
+            title: '',
+            place: '',
+            activity: '',
+            url: '',
+            map: '',
+            people: [{ name: '', time: '' }],
+          };
+          break;
+        case '個人卡片':
+          form.value = {
+            template: '3',
+            title: '',
+            description: '',
+            avatar: '',
+            back: '',
+            followUrl: '',
           };
           break;
       }
     }
     function peopleListPlus() {
-      if (form.value.people) form.value.people.push({ name: "", time: "" });
+      if (form.value.people) form.value.people.push({ name: '', time: '' });
     }
     return {
       form,
